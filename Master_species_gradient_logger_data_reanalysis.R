85af6637ada8e643bb94b54127c20ef32b3d7d89
mattgrobelny <mattgrobelny@gmail.com>
2016-08-16 00:34:19 -0500

    Updated formatting
    clearer formatting
    commented out unnecessary code

--- Master_species_gradient_logger_data_reanalysis.R
+++ Master_species_gradient_logger_data_reanalysis.R
@@ -1,12 +1,12 @@
 # Data reanalysis for the thermal gradient project
 # By: Mateusz Grobelny
 
-#Libraries
+########################################################################################################################################
+# Libraries
 library(ggplot2)
 library(RColorBrewer)
 library(fields)
 library(MASS)
-
 
 
 # Import data set 
@@ -15,118 +15,121 @@ Master_species_gradient_logger_data <- read.csv("~/Documents/OneDrive/Antarctica
 #set wd
 setwd("~/Documents/OneDrive/Antarctica Files/Data/Gradient Project/Hobodata/Large Tank/DATA_reanalysis")
 
-#Functions
+########################################################################################################################################
+## Functions
+# 
+# #Function1- Input column number for sp from Master dataset. Outputs graph of tempeature vs time.
+# make_line_graph_of<-function(x,size){
+# xvals=1:length(na.omit(Master_species_gradient_logger_data[,x]))
+# yvals=na.omit(Master_species_gradient_logger_data[,x])
+# x_n_y_vals=data.frame(xvals,yvals)
+# ggplot(data=x_n_y_vals, 
+#        aes(x=xvals, 
+#            y=yvals)
+#        )+
+#   geom_smooth(color="black")+
+#   ylim(-2, 2)+
+#   labs(title =paste("Thermal Gradient Data:", as.character(names(Master_species_gradient_logger_data)[x])), 
+#        x= parse(text=paste("Seconds (s","^-1",")")), 
+#        y= parse(text=paste("Temperature (C","^o",")")))+
+#   
+#   #add avg min/max temperatures from gradient 
+#   geom_hline(yintercept = 1.490, color="red")+
+#   geom_hline(yintercept = -1.130, color= "blue")+
+#   
+#   #Add avaerage line for data from the last 13 to 3 minutes  
+#   geom_hline(yintercept = mean(yvals[(length(yvals)-(15*60)):(length(yvals)-(5*60))]), color= "black",linetype="dashed")+
+#   
+#   #Change theme
+#   theme_bw()
+#   
+#   #save plot
+#   filename_x<-paste(names(Master_species_gradient_logger_data)[x],".png")
+#   filename_x<-gsub("\\s", "",filename_x)
+#   ggsave(filename=filename_x, width = size, height = size)
+# }
+# # Test for Funtion 1
+# make_line_graph_of(1,5)
+# 
+# #Function2- Input column number for sp from Master dataset. Outputs graph of tempeature vs time.
+# make_line_graph_of_15_to_5<-function(a,x,size){
+#   p=0
+#   b<-a
+#   vals<- data.frame(matrix(nrow=601, ncol= (x-b+2)))
+#   vals[,(x-b+2)]<-c(1:601)
+#   while (b<=x-b+2){
+#     for (z in 1:(x-b+1)){
+#       vals[,z]<-na.omit(Master_species_gradient_logger_data[,b])[(length(na.omit(Master_species_gradient_logger_data[,b]))-(15*60)):(length(na.omit(Master_species_gradient_logger_data[,b]))-(5*60))]
+#       b=b+1
+#     }}
+#   x_n_y_vals=vals
+#   #colnames(x_n_y_vals[,2:(x-b+2)])<-names(Master_species_gradient_logger_data)[b:x]
+#   #print(x_n_y_vals)
+#   
+#   #make plot
+#   plot<-ggplot(data=x_n_y_vals, 
+#          aes(x=x_n_y_vals[,(x-a+2)]))+
+#     ylim(-2, 2)+
+#     labs(title =paste("Thermal Gradient Data Between Last 15 to 5 minutes:", as.character(names(Master_species_gradient_logger_data)[x])), 
+#          x= parse(text=paste("Seconds (s","^-1",")")), 
+#          y= parse(text=paste("Temperature (C","^o",")")))+
+#     
+#     #add avg min/max temperatures from gradient 
+#     geom_hline(yintercept = 1.490, color="red")+
+#     geom_hline(yintercept = -1.130, color= "blue")+
+#     
+#     #Add avaerage line for data from the last 13 to 3 minutes  
+#     #geom_hline(yintercept = mean(yvals[(length(yvals)-(15*60)):(length(yvals)-(5*60))]), color= "black",linetype="dashed")+
+#     
+#     #Change theme
+#     theme_bw()
+#   b<-a
+#   print(x-b+2)
+#   while (b<=(x-b+2)){
+#     loop_input = paste("geom_smooth(aes(y=",x_n_y_vals[,b],",color='black')")
+#     print(loop_input)
+#     plot <- plot + eval(parse(text=loop_input))
+#     b<-1+b
+#   }
+#   #save plot
+#   # filename_x<-paste(names(Master_species_gradient_logger_data)[x],".png")
+#   # filename_x<-gsub("\\s", "",filename_x)
+#   # ggsave(filename=filename_x, width = size, height = size)
+# }
+# 
+# # Test for Funtion 2
+# make_line_graph_of_15_to_5(a=1,x=3,size=5)
+# 
+# #Function3- Histogram distribution.
+# make_line_graph_of_freq<-function(z,x,size){
+#   yvals=data.frame(na.omit(Master_species_gradient_logger_data[,x]))
+#   ggplot(data=yvals,aes(x=yvals[,1]))+
+#     geom_density2d()+
+#     xlim(-2, 2)+stat_function(fun = dnorm, colour = "red")+
+#     labs(title =paste("Thermal Gradient Data:", as.character(names(Master_species_gradient_logger_data)[x])), 
+#          y= parse(text=paste("# of Seconds Spent at Temperature (s","^-1",")")), 
+#          x= parse(text=paste("Temperature (C","^o",")")))+
+#     
+#     #add avg min/max temperatures from gradient 
+#     #geom_hline(yintercept = 1.490, color="red")+
+#     #geom_hline(yintercept = -1.130, color= "blue")+
+#     
+#     #Add avaerage line for data from the last 13 to 3 minutes  
+#     #geom_hline(yintercept = mean(yvals[(length(yvals)-(15*60)):(length(yvals)-(5*60))]), color= "black",linetype="dashed")+
+#     
+#     #Change theme
+#     theme_bw()
+#   #save plot
+#   filename_x<-paste(names(Master_species_gradient_logger_data)[x],".png")
+#   filename_x<-gsub("\\s", "",filename_x)
+#   ggsave(filename=filename_x, width = size, height = size)
+# }
+# # Test for Funtion 3
+# make_line_graph_of_freq(0.1,24,5)
 
-#Function1- Input column number for sp from Master dataset. Outputs graph of tempeature vs time.
-make_line_graph_of<-function(x,size){
-xvals=1:length(na.omit(Master_species_gradient_logger_data[,x]))
-yvals=na.omit(Master_species_gradient_logger_data[,x])
-x_n_y_vals=data.frame(xvals,yvals)
-ggplot(data=x_n_y_vals, 
-       aes(x=xvals, 
-           y=yvals)
-       )+
-  geom_smooth(color="black")+
-  ylim(-2, 2)+
-  labs(title =paste("Thermal Gradient Data:", as.character(names(Master_species_gradient_logger_data)[x])), 
-       x= parse(text=paste("Seconds (s","^-1",")")), 
-       y= parse(text=paste("Temperature (C","^o",")")))+
-  
-  #add avg min/max temperatures from gradient 
-  geom_hline(yintercept = 1.490, color="red")+
-  geom_hline(yintercept = -1.130, color= "blue")+
-  
-  #Add avaerage line for data from the last 13 to 3 minutes  
-  geom_hline(yintercept = mean(yvals[(length(yvals)-(15*60)):(length(yvals)-(5*60))]), color= "black",linetype="dashed")+
-  
-  #Change theme
-  theme_bw()
-  
-  #save plot
-  filename_x<-paste(names(Master_species_gradient_logger_data)[x],".png")
-  filename_x<-gsub("\\s", "",filename_x)
-  ggsave(filename=filename_x, width = size, height = size)
-}
-# Test for Funtion 1
-make_line_graph_of(1,5)
 
-#Function2- Input column number for sp from Master dataset. Outputs graph of tempeature vs time.
-make_line_graph_of_15_to_5<-function(a,x,size){
-  p=0
-  b<-a
-  vals<- data.frame(matrix(nrow=601, ncol= (x-b+2)))
-  vals[,(x-b+2)]<-c(1:601)
-  while (b<=x-b+2){
-    for (z in 1:(x-b+1)){
-      vals[,z]<-na.omit(Master_species_gradient_logger_data[,b])[(length(na.omit(Master_species_gradient_logger_data[,b]))-(15*60)):(length(na.omit(Master_species_gradient_logger_data[,b]))-(5*60))]
-      b=b+1
-    }}
-  x_n_y_vals=vals
-  #colnames(x_n_y_vals[,2:(x-b+2)])<-names(Master_species_gradient_logger_data)[b:x]
-  #print(x_n_y_vals)
-  
-  #make plot
-  plot<-ggplot(data=x_n_y_vals, 
-         aes(x=x_n_y_vals[,(x-a+2)]))+
-    ylim(-2, 2)+
-    labs(title =paste("Thermal Gradient Data Between Last 15 to 5 minutes:", as.character(names(Master_species_gradient_logger_data)[x])), 
-         x= parse(text=paste("Seconds (s","^-1",")")), 
-         y= parse(text=paste("Temperature (C","^o",")")))+
-    
-    #add avg min/max temperatures from gradient 
-    geom_hline(yintercept = 1.490, color="red")+
-    geom_hline(yintercept = -1.130, color= "blue")+
-    
-    #Add avaerage line for data from the last 13 to 3 minutes  
-    #geom_hline(yintercept = mean(yvals[(length(yvals)-(15*60)):(length(yvals)-(5*60))]), color= "black",linetype="dashed")+
-    
-    #Change theme
-    theme_bw()
-  b<-a
-  print(x-b+2)
-  while (b<=(x-b+2)){
-    loop_input = paste("geom_smooth(aes(y=",x_n_y_vals[,b],",color='black')")
-    print(loop_input)
-    plot <- plot + eval(parse(text=loop_input))
-    b<-1+b
-  }
-  #save plot
-  # filename_x<-paste(names(Master_species_gradient_logger_data)[x],".png")
-  # filename_x<-gsub("\\s", "",filename_x)
-  # ggsave(filename=filename_x, width = size, height = size)
-}
-
-# Test for Funtion 2
-make_line_graph_of_15_to_5(a=1,x=3,size=5)
-
-#Function3- Histogram distribution.
-make_line_graph_of_freq<-function(z,x,size){
-  yvals=data.frame(na.omit(Master_species_gradient_logger_data[,x]))
-  ggplot(data=yvals,aes(x=yvals[,1]))+
-    geom_density2d()+
-    xlim(-2, 2)+stat_function(fun = dnorm, colour = "red")+
-    labs(title =paste("Thermal Gradient Data:", as.character(names(Master_species_gradient_logger_data)[x])), 
-         y= parse(text=paste("# of Seconds Spent at Temperature (s","^-1",")")), 
-         x= parse(text=paste("Temperature (C","^o",")")))+
-    
-    #add avg min/max temperatures from gradient 
-    #geom_hline(yintercept = 1.490, color="red")+
-    #geom_hline(yintercept = -1.130, color= "blue")+
-    
-    #Add avaerage line for data from the last 13 to 3 minutes  
-    #geom_hline(yintercept = mean(yvals[(length(yvals)-(15*60)):(length(yvals)-(5*60))]), color= "black",linetype="dashed")+
-    
-    #Change theme
-    theme_bw()
-  #save plot
-  filename_x<-paste(names(Master_species_gradient_logger_data)[x],".png")
-  filename_x<-gsub("\\s", "",filename_x)
-  ggsave(filename=filename_x, width = size, height = size)
-}
-# Test for Funtion 3
-make_line_graph_of_freq(0.1,24,5)
-####################################################################
-#Data analysis
+########################################################################################################################################
+## Data analysis
 
 #Print a graph for each species per individual for all data
 #size for graphs values 5-10
@@ -142,11 +145,15 @@ for(i in 1:length(names(Master_species_gradient_logger_data))){
 # for(i in 1:length(names(Master_species_gradient_logger_data))){
 #   make_line_graph_of_freq(binwidth, i,size)
 # }
+
+########################################################################################################################################
 #Print state summary for each indiviual 
 b=c(1:27)
 stat_summary<-data.frame(summary(na.omit(Master_species_gradient_logger_data[,b])[(length(na.omit(Master_species_gradient_logger_data[,b]))-(15*60)):(length(na.omit(Master_species_gradient_logger_data[,b]))-(5*60))]))
 
-#Reorgnize data
+########################################################################################################################################
+## Reorgnize data
+
 #Ngib 
 Ngib_data<-c(Master_species_gradient_logger_data$Ngib1,Master_species_gradient_logger_data$Ngib2,Master_species_gradient_logger_data$Ngib3)
 
@@ -200,7 +207,7 @@ for (i in 22:27){
 # base
 
 ##########################################################################################################################################
-# BETTER Distribution of desnity of temperatures per species 
+## BETTER Distribution of desnity of temperatures per species 
 
 # x = the smoothness of the plot ( 0= raw ;; 2= smooth) 
 x=1
@@ -303,6 +310,8 @@ qqnorm(Ncor_bin$counts)  # skewed data
 
 
 ####################################################################################################
+#### DOES NOT WORK RIGHT 
+
 # Fuction to output expected poisson distribtion 
 poisson_expected<-function(x){
 set.seed(123)
@@ -376,6 +385,8 @@ all_species_no_Ngib_temp_data<-list(Than_temp=Than_temp,Cwil_temp=Cwil_temp,Lsq_
 kruskal.test(all_species_no_Ngib_temp_data) #fail to reject null, group means are statictially equal
 # still not what I want to test?
 # I want to test if each species has a random distribution or not!
+
+
 ####################################################################################################
 #For AFGP enabled temp disctibution
 # AFGP fotifed temps vs non AFGP fortified non parametric test Kruskal_wallis test
@@ -432,26 +443,30 @@ qqnorm(AFGP_Neg_data$AFGP_Neg) # does not look normal (values do not fall on a s
 AFGP_Pos.ecdf<-ecdf(AFGP_Pos_data$AFGP_Pos)
 AFGP_Neg.ecdf<-ecdf(AFGP_Neg_data$AFGP_Neg)
 
+## Plot individual CDF for AFGP Pos vs Neg
 plot(AFGP_Pos.ecdf,
      xlab = parse(text=paste("Temperature (C","^o",")")), 
      ylab = '', 
      main = 'CDF of AFGP Positive Species Temperature Distribution'
-    
-)
+    )
 mtext(text = expression(hat(F)[n](x)), side = 2, line = 2.5)
 
 plot(AFGP_Neg.ecdf,
      xlab = parse(text=paste("Temperature (C","^o",")")), 
      ylab = '', 
      main = 'CDF of AFGP Negative Species Temperature Distribution'
-     
-)
+     )
 mtext(text = expression(hat(F)[n](x)), side = 2, line = 2.5)
 
-#The KS-test uses the maximum vertical deviation between the two curves as the statistic D.
+############################################################################################################
+## Kolmogorov-Smirnov Tests
+# The KS-test uses the maximum vertical deviation between the two curves as the statistic D.
+
 KS_Test_AFGP_neg_vs_pos<-ks.test(AFGP_Pos_data$AFGP_Pos,AFGP_Neg_data$AFGP_Neg)
 
 ######## From KS test ---> Significant difference between the two distributions 
+
+############################################################################################################
 
 base3 <- ggplot(stack_all_species, aes(x=stack_all_species[,1],colour = AFGP_content))+
 
@@ -460,6 +475,8 @@ base3 <- ggplot(stack_all_species, aes(x=stack_all_species[,1],colour = AFGP_con
   xlim(-2, 2)+
   ylab(expression(hat(F)[n](x)))+
   xlab(parse(text=paste("Temperature (C","^o",")")))+
+  geom_vline(xintercept = 1.490, color="red")+
+  geom_vline(xintercept = -1.130, color= "blue")+
   theme_bw()
 
 base3
